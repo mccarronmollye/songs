23ac13247cf5862653488e303c438a8975d5d9ce
Molly McCarron <mccarronmollye@gmail.com>
2019-04-30 16:11:31 -0400

    added reducers, actions, SongList Component.

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import { selectSong } from '../actions'
+import SongList from './SongList'
+
+//named need curly braces
+
+const App = () => {
+  return (
+    <div>
+      <SongList />
+    </div>
+  )
+};
+
+
+export default App;
