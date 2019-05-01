4145ff5896a0e4818ff97d0ee2bf6ee2efac0816
Molly McCarron <mccarronmollye@gmail.com>
2019-05-01 12:26:20 -0400

    Can select a song from list of songs and view details of selected song

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'; //1
+import { selectSong } from '../actions'
 
 
 class SongList extends React.Component {
+  renderList(){
+
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+              >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      )
+    })
+  }
+
   render(){
-    return <div>SongList</div>
+    return <div className="ui divided list">{this.renderList()}</div>
   }
 }
+//updates state
+const mapStateToProps = (state) => {
+  return { songs: state.songs }
+}//2
+//going to take state and run to show up as props
 
-export default connect()(SongList);
+
+export default connect(mapStateToProps, { selectSong })(SongList);//3
